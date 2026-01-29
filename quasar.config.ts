150d26c989eb557bf1f51757ebbcdfe03edfe396
jonathan besinan <jonathan.besinan@gmail.com>
2026-01-29 20:49:38 +0800

    update dist

--- quasar.config.ts
+++ quasar.config.ts
@@ -56,7 +56,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/gomee-site/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
