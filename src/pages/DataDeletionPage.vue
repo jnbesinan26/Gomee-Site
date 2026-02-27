9f89c5ddc5354d1044e2515d415d446187a793bc
jonathan besinan <jonathan.besinan@gmail.com>
2026-02-27 23:33:46 +0800

    add data deletion page

---
+++ src/pages/DataDeletionPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center column q-pa-lg">
+    <base-container>
+      <q-card class="q-py-xl">
+        <q-card-section>
+          <h2 class="text-center text-info q-mb-none">Request Data Deletion</h2>
+          <p>
+            If you would like some or all of your data deleted, please email us at
+            <a href="mailto:support@gomee.app" class="text-info">support@gomee.app</a>.
+          </p>
+        </q-card-section>
+      </q-card>
+    </base-container>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { BaseContainer } from '@/components';
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,6 +9,7 @@ const routes: RouteRecordRaw[] = [
       { path: 'privacy-policy', component: () => import('pages/PrivacyPage.vue') },
       { path: 'terms', component: () => import('pages/TermsPage.vue') },
       { path: 'auth/action', component: () => import('pages/AuthActionPage.vue') },
+      { path: 'data-deletion', component: () => import('pages/DataDeletionPage.vue') },
     ],
   },
 
